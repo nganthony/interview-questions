1d0e4b042e465ed45be9d9688cf8aa7d53174d5e
anthonyng <ngcanthony@gmail.com>
2015-03-01 17:45:19 -0500

    Implemented recursive solution to Q8.4 of CTCI

--- src/recursion/StringPermutations.java
+++ src/recursion/StringPermutations.java
@@ -1,5 +1,7 @@
 package recursion;
 
+import java.util.ArrayList;
+
 /**
  * Solution to Q8.4 of Cracking the Coding Interview
  * @author Anthony
@@ -8,7 +10,11 @@ package recursion;
 public class StringPermutations {
 	public static void main(String [] args) {
 		String s = "abcd";
-		printPermutations(s);
+		ArrayList<String> permutations = getPermutations(s, 0);
+		
+		for(String permutation: permutations) {
+			System.out.println(permutation);
+		}		
 	}
 	
 	private static void printPermutationsHelper(String s, String currentString) {
@@ -28,5 +34,42 @@ public class StringPermutations {
 	
 	private static void printPermutations(String s) {
 		printPermutationsHelper(s, "");
+	}
+	
+	private static ArrayList<String> getPermutations(String s, int index) {
+		ArrayList<String> permutations = new ArrayList<String>();;
+		
+		if(s.length() == index) {
+			permutations.add("");
+		}
+		else {
+			ArrayList<String> oldPermutations = getPermutations(s, index + 1);
+			char first = s.charAt(index);
+			
+			for(int i = 0; i < oldPermutations.size(); i++) {
+				String oldPermutation = oldPermutations.get(i);
+				for(int j = 0; j <= oldPermutation.length(); j++) {
+					permutations.add(insertChar(oldPermutation, first, j));
+				}
+			}
+ 		}
+		
+		return permutations;
+	}
+	
+	private static String insertChar(String s, char c, int index) {
+		String newString;
+		
+		if(index == 0) {
+			newString = c + s;
+		}
+		else if(index == s.length()) {
+			newString = s + c;
+		}
+		else {
+			newString = s.substring(0, index) + c + s.substring(index);
+		}
+		
+		return newString;
 	}
 }
